c3258efa1d6b96c2207195e609fe09c8007d4032
y.yoshioka <y.yoshioka@ashita-team.com>
2019-03-26 17:57:19 +0900

    キャラクターの攻撃力と防御力を計算するクラス群の作成

--- calculators/warrior_calculators/warrior_attack_calculator.rb
+++ calculators/warrior_calculators/warrior_attack_calculator.rb
@@ -2,13 +2,21 @@ require_relative "../attack_calculator"
 
 class WarriorAttackCalculator < AttackCalculator
 
-  def calculate(*attributes)
-    super + correction
+private
+  def correction
+    equipment_correction
   end
 
-  def correction
+  def equipment_correction
     return 20 if equipment.name == "sword"
     0
   end
+
+# 武器や各属性に補正値を持たせる
+
 #   この箇所だけのために、各クラスに@nameインスタンスを作成、直したい。
 end
+
+# 分岐を使わざるを得ない状況、吉岡くんの限界に陥った時
+# 実質、ほぼ０
+# if文を消すため、多態性、classにしていく。
--- calculators/warrior_calculators/warrior_defence_calculator.rb
+++ calculators/warrior_calculators/warrior_defence_calculator.rb
@@ -2,12 +2,16 @@ require_relative "../defence_calculator"
 
 class WarriorDefenceCalculator < DefenceCalculator
 
-  def calculate(*attributes)
-    super + correction
+private
+  def correction
+    element_correction
   end
 
-  def correction
+  def element_correction
     return 10 if element.name == "water"
     0
   end
 end
+# 分岐を使わざるを得ない状況、吉岡くんの限界に陥った時
+# 実質、ほぼ０
+# if文を消すため、多態性、classにしていく。
