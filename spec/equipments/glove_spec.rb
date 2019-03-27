9963db53a907de9476fbdbd8c4cee8a0f2b14416
y.yoshioka <y.yoshioka@ashita-team.com>
2019-03-27 16:00:00 +0900

    test: Gloveクラスのテストを追加

---
+++ spec/equipments/glove_spec.rb
@@ -0,0 +1,12 @@
+require_relative "../../equipments/glove.rb"
+require_relative "../../jobs/warrior.rb"
+
+describe "Glove" do
+  let(:glove) { Glove.new }
+  it "攻撃力が20上昇する" do
+    expect(glove.offence).to eq(20)
+  end
+  it "防御力が20上昇する" do
+    expect(glove.defence).to eq(20)
+  end
+end
