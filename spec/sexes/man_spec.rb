1d4c4fe9ddbd57e549e44471873af5dc49899359
y.yoshioka <y.yoshioka@ashita-team.com>
2019-04-04 15:08:52 +0900

    refactor: letでbindするときのインスタンス名が複数形のままだったので、単数形に変更

--- spec/sexes/man_spec.rb
+++ spec/sexes/man_spec.rb
@@ -4,30 +4,30 @@ require_relative '../../jobs/wizard'
 require_relative '../../jobs/fighter'
 
 describe 'Man' do
-  let(:men) { Man.new }
+  let(:man) { Man.new }
   it '攻撃力が５上昇する' do
-    expect(men.attack).to eq(5)
+    expect(man.attack).to eq(5)
   end
   it '防御力は変わらない' do
-    expect(men.defence).to eq(0)
+    expect(man.defence).to eq(0)
   end
   context '職業が異なる場合' do
     let(:element) { Wind.new }
     let(:equipment) { Equipment.new }
     context '戦士の場合' do
-      let(:warrior) { Warrior.new(sex: men, element: element, equipment: equipment) }
+      let(:warrior) { Warrior.new(sex: man, element: element, equipment: equipment) }
       it 'スキルがギガスラッシュ' do
         expect(warrior.skill).to eq 'ギガスラッシュ'
       end
     end
     context '魔法使いの場合' do
-      let(:wizard) { Wizard.new(sex: men, element: element, equipment: equipment) }
+      let(:wizard) { Wizard.new(sex: man, element: element, equipment: equipment) }
       it 'スキルがベギラマ' do
         expect(wizard.skill).to eq 'ベギラマ'
       end
     end
     context '武闘家の場合' do
-      let(:fighter) { Fighter.new(sex: men, element: element, equipment: equipment) }
+      let(:fighter) { Fighter.new(sex: man, element: element, equipment: equipment) }
       it 'スキルがブースト' do
         expect(fighter.skill).to eq 'ブースト'
       end
