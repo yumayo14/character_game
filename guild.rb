902e787d4fafaf644e8538f7b2d05f10b2cb219b
y.yoshioka <y.yoshioka@ashita-team.com>
2019-04-15 16:04:35 +0900

    feat: キャラクターの強さ判定機能の呼び出し元を作成

--- guild.rb
+++ guild.rb
@@ -1,10 +1,23 @@
 require_relative 'job.rb'
 require_relative 'character_factory.rb'
+require_relative 'character_checker.rb'
 
 class Guild
   attr_reader :member
   def initialize
     @member = all_character
+  end
+
+  def best_characters
+    CharacterChecker.new(member).best_characters
+  end
+
+  def best_attackers
+    CharacterChecker.new(member).best_attackers
+  end
+
+  def best_defenders
+    CharacterChecker.new(member).best_defenders
   end
 
   private
