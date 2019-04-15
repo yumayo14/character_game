5f62c75ff541d486d6b79c7d7f913c9342cbe8fa
y.yoshioka <y.yoshioka@ashita-team.com>
2019-04-15 16:17:40 +0900

    test: キャラクターの強さ判定の呼び出しが成功しているかをテスト

--- spec/guild_spec.rb
+++ spec/guild_spec.rb
@@ -2,19 +2,75 @@ require_relative '../guild'
 
 describe Guild do
   let(:guild) { Guild.new }
-  it 'キャラクターの数は54人' do
-    expect(guild.member.count).to eq 54
+  describe 'キャラクターの数は54人' do
+    subject { guild.member.count }
+    it { is_expected.to eq 54 }
   end
-  it '1人目は剣を装備した風属性の男性武闘家' do
-    expect(guild.member.first.class).to eq Fighter
-    expect(guild.member.first).to have_attributes(sex: be_an_instance_of(Man))
-    expect(guild.member.first).to have_attributes(element: be_an_instance_of(Wind))
-    expect(guild.member.first).to have_attributes(equipment: be_an_instance_of(Sword))
+  describe '1人目は剣を装備した風属性の男性武闘家' do
+    subject { guild.member.first }
+    it { is_expected.to be_an_instance_of(Fighter) }
+    it { is_expected.to have_attributes(sex: be_an_instance_of(Man)) }
+    it { is_expected.to have_attributes(element: be_an_instance_of(Wind)) }
+    it { is_expected.to have_attributes(equipment: be_an_instance_of(Sword)) }
   end
-  it '54人目はグローブを装備した雷属性の女性魔法使い' do
-    expect(guild.member.last.class).to eq Wizard
-    expect(guild.member.last).to have_attributes(sex: be_an_instance_of(Woman))
-    expect(guild.member.last).to have_attributes(element: be_an_instance_of(Thunder))
-    expect(guild.member.last).to have_attributes(equipment: be_an_instance_of(Glove))
+  describe '54人目はグローブを装備した雷属性の女性魔法使い' do
+    subject { guild.member.last }
+    it { is_expected.to be_an_instance_of(Wizard) }
+    it { is_expected.to have_attributes(sex: be_an_instance_of(Woman)) }
+    it { is_expected.to have_attributes(element: be_an_instance_of(Thunder)) }
+    it { is_expected.to have_attributes(equipment: be_an_instance_of(Glove)) }
+  end
+  describe '#best_characters' do
+    describe 'キャラクターの数は2人' do
+      subject { guild.best_characters.count }
+      it { is_expected.to eq 2 }
+    end
+    describe '1人目は剣を装備した水属性の男性戦士' do
+      subject { guild.best_characters.first }
+      it { is_expected.to be_an_instance_of(Warrior) }
+      it { is_expected.to have_attributes(sex: be_an_instance_of(Man)) }
+      it { is_expected.to have_attributes(element: be_an_instance_of(Water)) }
+      it { is_expected.to have_attributes(equipment: be_an_instance_of(Sword)) }
+    end
+    describe '2人目は剣を装備した水属性の女性戦士' do
+      subject { guild.best_characters.last }
+      it { is_expected.to be_an_instance_of(Warrior) }
+      it { is_expected.to have_attributes(sex: be_an_instance_of(Woman)) }
+      it { is_expected.to have_attributes(element: be_an_instance_of(Water)) }
+      it { is_expected.to have_attributes(equipment: be_an_instance_of(Sword)) }
+    end
+  end
+  describe '#best_attackers' do
+    describe 'キャラクターの数は2人' do
+      subject { guild.best_attackers.count }
+      it { is_expected.to eq 2 }
+    end
+    describe '1人目は剣を装備した雷属性の男性戦士' do
+      subject { guild.best_attackers.first }
+      it { is_expected.to be_an_instance_of(Warrior) }
+      it { is_expected.to have_attributes(sex: be_an_instance_of(Man)) }
+      it { is_expected.to have_attributes(element: be_an_instance_of(Thunder)) }
+      it { is_expected.to have_attributes(equipment: be_an_instance_of(Sword)) }
+    end
+    describe '2人目は杖を装備した雷属性の男性魔法使い' do
+      subject { guild.best_attackers.last }
+      it { is_expected.to be_an_instance_of(Wizard) }
+      it { is_expected.to have_attributes(sex: be_an_instance_of(Man)) }
+      it { is_expected.to have_attributes(element: be_an_instance_of(Thunder)) }
+      it { is_expected.to have_attributes(equipment: be_an_instance_of(Stick)) }
+    end
+  end
+  describe '#best_defenders' do
+    describe 'キャラクターの数は1人' do
+      subject { guild.best_defenders.count }
+      it { is_expected.to eq 1 }
+    end
+    describe 'グローブを装備した風属性の女性武闘家' do
+      subject { guild.best_defenders.first }
+      it { is_expected.to be_an_instance_of(Fighter) }
+      it { is_expected.to have_attributes(sex: be_an_instance_of(Woman)) }
+      it { is_expected.to have_attributes(element: be_an_instance_of(Wind)) }
+      it { is_expected.to have_attributes(equipment: be_an_instance_of(Glove)) }
+    end
   end
 end
