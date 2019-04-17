47b954489d5bf968326c81adf1ba66632b20a466
y.yoshioka <y.yoshioka@ashita-team.com>
2019-04-17 09:21:24 +0900

    test: 杖を装備した場合の補正値をテスト

--- spec/equipments/stick_spec.rb
+++ spec/equipments/stick_spec.rb
@@ -8,4 +8,13 @@ describe 'Stick' do
   it '防御力が10上昇する' do
     expect(stick.defence).to eq 10
   end
+  it '戦士が装備した場合、補正値は0' do
+    expect(stick.warrior_correction).to eq 0
+  end
+  it '魔法使いが装備した場合、補正値で更に20上昇' do
+    expect(stick.wizard_correction).to eq 20
+  end
+  it '武闘家が装備した場合、補正値は0' do
+    expect(stick.fighter_correction).to eq 0
+  end
 end
