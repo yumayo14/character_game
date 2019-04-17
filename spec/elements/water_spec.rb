aa202fc546f31b5e37db75cddac6d9d580b1174b
y.yoshioka <y.yoshioka@ashita-team.com>
2019-04-17 09:45:33 +0900

    test: 属性が水の場合の補正値をテスト

--- spec/elements/water_spec.rb
+++ spec/elements/water_spec.rb
@@ -8,4 +8,13 @@ describe 'Water' do
   it '防御力が5上昇する' do
     expect(water.defence).to eq 5
   end
+  it '戦士が水属性の場合、補正値で更に10上昇' do
+    expect(water.warrior_correction).to eq 10
+  end
+  it '魔法使いが水属性の場合、補正値は0' do
+    expect(water.wizard_correction).to eq 0
+  end
+  it '武闘家が水属性の場合、補正値は0' do
+    expect(water.fighter_correction).to eq 0
+  end
 end
