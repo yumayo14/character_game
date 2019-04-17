4c1c41bd55b886b0e6376ef910665131d29f372a
y.yoshioka <y.yoshioka@ashita-team.com>
2019-04-17 09:46:05 +0900

    test: 属性が雷の場合の補正値をテスト

--- spec/elements/thunder_spec.rb
+++ spec/elements/thunder_spec.rb
@@ -8,4 +8,13 @@ describe 'Thunder' do
   it '防御力は変わらない' do
     expect(thunder.defence).to eq 0
   end
+  it '戦士が雷属性の場合、補正値は0' do
+    expect(thunder.warrior_correction).to eq 0
+  end
+  it '魔法使いが雷属性の場合、補正値で更に5上昇' do
+    expect(thunder.wizard_correction).to eq 5
+  end
+  it '武闘家が雷属性の場合、補正値は0' do
+    expect(thunder.fighter_correction).to eq 0
+  end
 end
